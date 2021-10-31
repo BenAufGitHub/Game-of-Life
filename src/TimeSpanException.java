a9e53be2f21d9b33f959eb9b023ee7dce8206652
Ben Mette <benme04@gmail.com>
2021-10-31 18:49:56 +0100

    added Exception for tick-speed

--- src/TimeSpanException.java
+++ src/TimeSpanException.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class TimeSpanException {
+public class TimeSpanException extends Exception{
+    public TimeSpanException(int val, String problem){
+        super("Your timespan " + val + " is " + problem + ". The timespan reaches from 300 to 12 000 Milliseconds");
+    }
 }
