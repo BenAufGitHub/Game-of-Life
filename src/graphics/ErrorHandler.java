9c9d45d9545e390d776294ccb8dc796128b08f43
Ben Mette <benme04@gmail.com>
2021-11-27 17:46:38 +0100

    error window done

--- src/graphics/ErrorHandler.java
+++ src/graphics/ErrorHandler.java
@@ -1,8 +1,14 @@
 package graphics;
 
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.WindowConstants;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.WindowEvent;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
@@ -15,21 +21,32 @@ public class ErrorHandler {
         informUser(e, errorCode);
         window.setVisible(false);
         window.dispose();
-        System.exit(errorCode);
     }
 
     /*
     opens a window for insightful error message
      */
     private static void informUser(Exception e, int errorCode) {
-        JFrame frame = new JFrame("An issue occurred");
-        JTextArea text = new JTextArea();
+        JFrame frame = new ErrorFrame("An issue occurred", errorCode);
+        JTextArea text = new JTextArea("", 3, 20);
+        JScrollPane scroll = new JScrollPane (text, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
         text.append(getText(e, errorCode));
+        text.setLineWrap(true);
+        text.setWrapStyleWord(true);
+        text.setEditable(false);
+        text.setBackground(Color.DARK_GRAY);
+        text.setForeground(Color.CYAN);
+
+        scroll.setPreferredSize(new Dimension(380,380));
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
         frame.setLocationRelativeTo(null);
-        frame.setSize(200,400);
+        frame.setSize(400,400);
+        frame.getContentPane().setBackground(Color.BLACK);
+
+        frame.add(scroll);
         frame.setVisible(true);
     }
 
@@ -40,13 +57,15 @@ public class ErrorHandler {
     private static String getText(Exception e, int errorCode) {
         StringBuilder text = new StringBuilder();
         text.append(getErrorMessage(errorCode));
-        text.append("\nYou can review the whole java error below:\n");
+        text.append("\nYou can review the whole java error below:\n\n");
 
         StringWriter stackTraceWriter = new StringWriter();
         e.printStackTrace(new PrintWriter(stackTraceWriter));
-        text.append(e.toString() + "\n" + stackTraceWriter.toString());
+        text.append(stackTraceWriter.toString());
+
         return text.toString();
     }
+
 
     /*
     custom error message for each code, done w switch-case
@@ -60,5 +79,25 @@ public class ErrorHandler {
         }
     }
 
+
+    /*
+    in use to override processWindowEvent, which lets us exit the sys with the proper exit code
+     */
+    static class ErrorFrame extends JFrame{
+        int errorCode;
+
+        public ErrorFrame(String s, int errorcode){
+            super(s);
+            this.errorCode = errorcode;
+        }
+
+        @Override
+        public void processWindowEvent(WindowEvent e) {
+            if (e.getID() == WindowEvent.WINDOW_CLOSING) {
+                dispose();
+                System.exit(errorCode);
+            }
+        }
+    }
 
 }
