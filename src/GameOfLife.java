76c65155cfc2ee65569a991f679da1f85e385b62
Ben Mette <benme04@gmail.com>
2021-12-05 14:05:01 +0100

    game class

---
+++ src/GameOfLife.java
@@ -0,0 +1,23 @@
+import structure.Game;
+import structure.Output;
+
+public class GameOfLife extends Game {
+    public GameOfLife(Output op) {
+        super(op);
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void clicked(int x, int y) {
+
+    }
+
+    @Override
+    protected void act() {
+
+    }
+}
