29f59b9c1e4370091091d0780bb3ac33b2153767
Ben Mette <benme04@gmail.com>
2021-12-14 14:39:46 +0100

    comments

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -1,21 +1,21 @@
 import structure.*;
 
-import javax.swing.*;
 import java.awt.Color;
-import java.awt.Image;
 import java.util.HashMap;
-import java.util.HashSet;
 
 public class GameOfLife extends Game {
-    private final static Image circle = new ImageIcon("resources//black_circle.png").getImage();
-    private final static Color tracked = Color.RED;
     private final static Blueprint live = new Blueprint(Color.WHITE, null);
-    private final static Blueprint die  = new Blueprint(Color.RED, null);
     private final static Blueprint clear = new Blueprint(Color.GRAY, null);
-    private final static Blueprint track = new Blueprint(tracked);
 
     CellTracker cellTracker;
 
+
+    /**
+     * the coordinates need to be in bounds of output grid, else the errorhandler takes over
+     * @param op -> output
+     * @param x
+     * @param y
+     */
     public GameOfLife(Output op, int x, int y) {
         super(op);
         if(x > op.gridWidth() || y > op.gridHeight()){
@@ -29,12 +29,22 @@ public class GameOfLife extends Game {
         this.cellTracker = new CellTracker(new Grid(x,y));
     }
 
+
+    /**
+     * TODO not in use, comes with addition of reset button
+     */
     @Override
     public void reset() {
         getCellTracker().setGrid(new Grid(getOutput().gridWidth(), getOutput().gridHeight()));
         getOutput().clear();
     }
 
+
+    /**
+     * this will be called if the output is clicked at specific coordinates from grid, so that the game can react to that
+     * @param x
+     * @param y
+     */
     @Override
     public void clicked(int x, int y) {
         getCellTracker().clicked(x,y);
@@ -49,6 +59,10 @@ public class GameOfLife extends Game {
     }
 
 
+    /**
+    lets Celltracker update grid and log the changes, then outputs it to the Output
+     TODO update logging into a queue
+     */
     @Override
     protected void act() {
         getCellTracker().act();
