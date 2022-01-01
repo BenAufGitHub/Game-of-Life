04dfdc48d7e2d91dfbe142354c487cf3394e45c5
Ben Mette <benme04@gmail.com>
2022-01-01 15:51:11 +0100

    save button opens textpromt for name

--- src/gol_extension/SaveExtension.java
+++ src/gol_extension/SaveExtension.java
@@ -3,13 +3,18 @@ package gol_extension;
 import structure.ErrorHandler;
 import structure.GUI;
 import tools.ChoiceWindow;
+import tools.EntryPopUp;
+import tools.EntryVerification;
 import tools.SaveManager;
 
 import javax.swing.JButton;
 import java.awt.Point;
 import java.io.IOException;
 
-public class SaveExtension extends GUIExtensioner{
+public class SaveExtension extends GUIExtensioner implements EntryVerification{
+
+    EntryPopUp popUp = null;
+
     public SaveExtension(GUI gui, GameOfLife game) {
         super(gui, game);
     }
@@ -25,6 +30,8 @@ public class SaveExtension extends GUIExtensioner{
     }
 
 
+    //------------------------------------------ saving -----------------------------------------------------------------
+
     /**
      * params: x/y = location
      */
@@ -38,6 +45,88 @@ public class SaveExtension extends GUIExtensioner{
         });
         return save;
     }
+
+
+    public void onSave(){
+        String saveName = nameFile();
+        if(saveName != null)
+            save(saveName);
+    }
+
+
+    public void save(String filename){
+        int width = getGame().getWidth();
+        int height = getGame().getHeight();
+
+        Point[] points = new Point[width * height];
+        int index = 0;
+        for(int x=0; x< width; x++){
+            for(int y=0; y< height; y++){
+                if(cellAlive(x,y))
+                    points[index++] = new Point(x, y);
+            }
+        }
+        SaveManager.save(filename, points);
+    }
+
+
+    private String nameFile(){
+        popUp = new EntryPopUp((GUI) getGame().getOutput());
+        popUp.setEntryVerification(this);
+        popUp.setTitle("Saving Cells");
+        popUp.write("Enter your Save-name");
+        popUp.setVisible(true);
+        // Thread now awaits the pop-up to be closed
+
+        if(popUp.noResult())
+            return null;
+        return popUp.getResult();
+    }
+
+
+    /**
+     * overriding from entry Verification
+     * String will be checked if it meets requirements (to be a save name)
+     * if yes -> popUp closes and writes the String into its result (getResult())
+     */
+    @Override
+    public boolean isValidEntry(String input) {
+        input = input.strip();
+        if(!isValidNameLength(input)){
+            popUp.write("Name should have between 5 and 20 Characters.");
+            return false;
+        }
+        if(!onlyValidCharacters(input)){
+            popUp.write("Name can only contain 'a-z', 'A-Z', '0-9' and '_'.");
+            return false;
+        }
+        return true;
+    }
+
+
+    /**
+     * in regard to Save Names.
+     */
+    private boolean isValidNameLength(String s){
+        return s.length() >= 5 && s.length() <= 20;
+    }
+
+
+    /**
+     * Checks if save name has bad characters such as '*' or ';'.
+     * Accepts letters, digits and underscore.
+     */
+    private boolean onlyValidCharacters(String word) {
+        for(Character c : word.toCharArray()){
+            if(!(Character.isLetter(c) || Character.isDigit(c) || c == '_'))
+                return false;
+        }
+        return true;
+    }
+
+
+
+    //------------------------------------ loading ------------------------------------------------------------
 
 
     /**
@@ -68,29 +157,6 @@ public class SaveExtension extends GUIExtensioner{
             ErrorHandler.catchError(getGUI(), ex, 6);
         }
     }
-
-
-    public void onSave(){
-        save("save");
-        //TODO prompt
-    }
-
-
-    public void save(String filename){
-        int width = getGame().getWidth();
-        int height = getGame().getHeight();
-
-        Point[] points = new Point[width * height];
-        int index = 0;
-        for(int x=0; x< width; x++){
-            for(int y=0; y< height; y++){
-                if(cellAlive(x,y))
-                    points[index++] = new Point(x, y);
-            }
-        }
-        SaveManager.save(filename, points);
-    }
-
 
 
     public void load(String filename) throws IOException {
