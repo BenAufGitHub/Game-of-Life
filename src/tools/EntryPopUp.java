e06d85d889499100cceff5f5877701c7d1caf645
Ben Mette <benme04@gmail.com>
2022-01-01 15:49:28 +0100

    new Textprompt tool (to use for save naming)

---
+++ src/tools/EntryPopUp.java
@@ -0,0 +1,90 @@
+package tools;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class EntryPopUp  extends PopUp<String>{
+
+    private EntryVerification verifier = null;
+    private JPanel container;
+    private JButton submit;
+    private JTextField textField;
+    private JLabel messageBox;
+    private int minimumWidth = 150;
+
+    public EntryPopUp(JFrame parent) {
+        super(parent);
+
+        this.setLayout(new BorderLayout());
+        this.setLocationRelativeTo(parent);
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        this.container = new JPanel();
+        this.submit = new JButton("submit");
+        this.textField = new JTextField();
+        this.messageBox = new JLabel();
+
+        messageBox.setBorder(new EmptyBorder(4, 4, 8, 4));
+        messageBox.setPreferredSize(new Dimension(minimumWidth, 30));
+        messageBox.setText("Huhu \\^u^/`");
+
+        submit.setBorder(new EmptyBorder(0, 4, 4, 4));
+        submit.setFocusable(false);
+        submit.addActionListener( e -> {
+            sendText();
+        });
+
+        textField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_ENTER){
+                    sendText();
+                }
+            }
+
+        });
+
+        this.add(messageBox, BorderLayout.NORTH);
+        this.add(textField, BorderLayout.CENTER);
+        this.add(submit, BorderLayout.EAST);
+        this.pack();
+
+    }
+
+    public void setEntryVerification(EntryVerification verifier){
+        this.verifier = verifier;
+    }
+
+
+    public void write(String message){
+        messageBox.setText(message);
+        adjustMessageBoxWidth();
+        pack();
+    }
+
+
+    private void sendText(){
+        String input = textField.getText();
+        if(verifier == null)
+            return;
+        if(!verifier.isValidEntry(input))
+            return;
+        setResult(textField.getText());
+        this.dispose();
+    }
+
+
+    private void adjustMessageBoxWidth(){
+        FontMetrics fm = messageBox.getFontMetrics(messageBox.getFont());
+        String text = messageBox.getText();
+
+        int width = fm.stringWidth(text) +10;
+        int height = 30;
+
+        width = Math.max(width, minimumWidth);
+        messageBox.setPreferredSize(new Dimension(width, height));
+    }
+}
