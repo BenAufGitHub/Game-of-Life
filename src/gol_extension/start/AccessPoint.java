ea8f703504a658a83b0eecfc0b0d2c1653ea4312
Ben Mette <benme04@gmail.com>
2022-01-04 00:34:41 +0100

    finalized gol's main method

--- src/gol_extension/start/AccessPoint.java
+++ src/gol_extension/start/AccessPoint.java
@@ -6,38 +6,31 @@ import gol_extension.updates.UpdaterRed;
 import structure.Selector;
 import structure.Settings;
 import tools.SaveManager;
-import worlds.HeaderFWindow;
 
-import javax.swing.SwingConstants;
 import java.awt.Color;
 import java.io.IOException;
 
 public class AccessPoint {
 
     public static void main(String[] args) throws IOException {
+        // enter the name of a save in the resources folder
+        final String save_name = null;
 
+        // Set up Settings, Window and Game.
         Settings settings = new Settings(Color.GRAY, true);
         WindowGOL window = new WindowGOL(100, 100, settings);
         ExtendedGOL gol = new ExtendedGOL(window, 100, 100, window);
 
-        Updater colouring = new UpdaterClean(window);
-        String save_name = "save.txt";
+        // Choose UpdaterClean or UpdaterRed or a custom Updater.
+        Updater colouring = new UpdaterRed(window);
 
-        gol.setUpdater(colouring);                                        //determines colouring
+        // determines colouring
+        gol.setUpdater(colouring);
 
+        // loads a save
         if(save_name != null)
-            new Selector(gol).selectAll(SaveManager.get(save_name));         // loads a save
+            new Selector(gol).selectAll(SaveManager.get(save_name));
 
         window.setVisible(true);
     }
 }
-
-
-//TODO
-//  make method to add buttons
-//  set Speed for Subclasses of FWindow
-//  make other games and compare time + optimal solution
-//  implement save button
-//  implement error output
-//  update preselect class
-//  plot the references onto paper
