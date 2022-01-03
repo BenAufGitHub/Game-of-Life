6f4651288272818aee31d12af664456cbde4192d
Ben Mette <benme04@gmail.com>
2022-01-03 17:20:24 +0100

    creating coherency between run specific window and run specific GOL

--- src/gol_extension/start/ExtendedGOL.java
+++ src/gol_extension/start/ExtendedGOL.java
@@ -1,12 +1,13 @@
-package gol_extension;
+package gol_extension.start;
 
-import structure.Output;
+import gol_extension.structure.GameOfLife;
 import structure.Printer;
 
 /**
  * adding a count system to GOL, output is shown by Printer
+ * adding saving and loading to GOL
  */
-public class CountingGOL extends GameOfLife{
+public class ExtendedGOL extends GameOfLife {
     private String outline = "The Game Of Life / Generation: ";
     private int count = 1;
     private Printer counter;
@@ -14,23 +15,47 @@ public class CountingGOL extends GameOfLife{
 
     /**
      * the coordinates need to be in bounds of output grid, else the Error-Handler takes over
-     *
-     * @param op -> output
-     * @param x
-     * @param y
      */
-    public CountingGOL(Output op, int x, int y) {
-        super(op, x, y);
-        counter = null;
+    public ExtendedGOL(WindowGOL output, int x, int y, Printer countPrinter) {
+        super(output, x, y);
+        counter = countPrinter;
+        initSaveModule(output);
+        initGameSpeed(output);
+
+        if(counter != null)
+            counter.write(outline + 1);
     }
 
-    public CountingGOL(Output op, int x, int y, Printer countPrinter) {
-        super(op, x, y);
-        counter = countPrinter;
+
+    private void initGameSpeed(WindowGOL output) {
+        //sets output-configs for slow, normal, fast and supersonic speed on Button click
+        output.configureSpeedButton(800, 300, 90, 35);
     }
+
+
+    private void initSaveModule(WindowGOL window) {
+        LoadButton load = new LoadButton(this);
+        SaveButton save = new SaveButton(this);
+
+        load.setBounds(170, 700, 80, 25);
+        load.setFocusable(false);
+
+        save.setBounds(70, 700, 80, 25);
+        save.setFocusable(false);
+
+        window.addButton(save, false);
+        window.addButton(load, false);
+    }
+
+
+
+    // ------------------------ counting ----------------------------------------
+
 
     public void setCountPrinter(Printer printer){
         this.counter = printer;
+        if(counter != null)
+            counter.write(outline + 1);
     }
 
     @Override
