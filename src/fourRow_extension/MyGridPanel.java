8b8d604f19ab564c53e868fafd0dfd08f17fda3c
Ben Mette <benme04@gmail.com>
2022-01-17 19:55:57 +0100

    Optimizing recognition of long mouse Clicks

--- src/fourRow_extension/MyGridPanel.java
+++ src/fourRow_extension/MyGridPanel.java
@@ -5,11 +5,14 @@ import structure.Selector;
 import structure.Settings;
 
 import javax.swing.BorderFactory;
+import javax.swing.JLabel;
 import java.awt.Color;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class MyGridPanel extends GridPanel implements MouseListener {
+
+    private boolean pressedCellNotExited = false;
 
     public MyGridPanel(int horizontal, int vertical, Settings settings, Selector selector) {
         super(horizontal, vertical, settings, selector);
@@ -18,13 +21,36 @@ public class MyGridPanel extends GridPanel implements MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        super.mousePressed(e);
+
     }
 
     @Override
-    public void mousePressed(MouseEvent e){ }
+    public void mousePressed(MouseEvent e){
+        setInCellStatus(true);
+    }
 
     @Override
     public void mouseExited(MouseEvent e) {
+        setInCellStatus(false);
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e){
+        JLabel label = (JLabel) e.getSource();
+        if(!isPressedCellNotExited())
+            return;
+        super.mousePressed(e);
+        setInCellStatus(false);
+    }
+
+
+    // --------------------------------- private methods ----------------------------------------------
+
+    private synchronized boolean isPressedCellNotExited(){
+        return pressedCellNotExited;
+    }
+
+    private synchronized void setInCellStatus(boolean inPressedCell){
+        this.pressedCellNotExited = inPressedCell;
     }
 }
